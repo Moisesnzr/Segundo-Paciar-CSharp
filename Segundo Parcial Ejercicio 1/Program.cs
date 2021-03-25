bb7d6ea035d4eb5e7184e72d18771d25c1714385
Moisesnzr <moisesnzr.rd@gmail.com>
2021-03-25 00:39:49 -0400

    Segundo Parcial

--- Segundo Parcial Ejercicio 1/Program.cs
+++ Segundo Parcial Ejercicio 1/Program.cs
@@ -2,158 +2,287 @@
 
 namespace Segundo_Parcial_Ejercicio_1
 {
-    public class Productos
+class producto
+{
+    public string nombre { get; set; }
+    public int precio { get; set; }
+    public int existencia { get; set; }
+}
+
+    class Program
     {
-        struct Datproductos
-        {
-            public int opcion;
-            public string nombre;
-            public double precio;
-            public int existencia;
-        }
-
-        private int opcompra;
-
-        Datproductos[] aProductos = new Datproductos[9];
-
-        public void speproduct()
-        {
-            aProductos[0].opcion = 0;
-            aProductos[0].nombre = "Galleta";
-            aProductos[0].precio = 5;
-            aProductos[0].existencia = 10;
-
-            aProductos[1].opcion = 1;
-            aProductos[1].nombre = "Agua";
-            aProductos[1].precio = 10;
-            aProductos[1].existencia = 10;
-
-            aProductos[2].opcion = 2;
-            aProductos[2].nombre = "Jugo";
-            aProductos[2].precio = 50;
-            aProductos[2].existencia = 10;
-
-            aProductos[3].opcion = 3;
-            aProductos[3].nombre = "Refresco";
-            aProductos[3].precio = 15;
-            aProductos[3].existencia = 10;
-
-            aProductos[4].opcion = 4;
-            aProductos[4].nombre = "Frito Lay";
-            aProductos[4].precio = 50;
-            aProductos[4].existencia = 10;
-
-            aProductos[5].opcion = 5;
-            aProductos[5].nombre = "360 Energy";
-            aProductos[5].precio = 100;
-            aProductos[5].existencia = 10;
-
-            aProductos[6].opcion = 6;
-            aProductos[6].nombre = "Vive100";
-            aProductos[6].precio = 100;
-            aProductos[6].existencia = 10;
-
-            aProductos[7].opcion = 7;
-            aProductos[7].nombre = "Mani";
-            aProductos[7].precio = 5;
-            aProductos[7].existencia = 10;
-
-            aProductos[8].opcion = 8;
-            aProductos[8].nombre = "Hershey's";
-            aProductos[8].precio = 50;
-            aProductos[8].existencia = 10;
-
-            aProductos[9].opcion = 9;
-            aProductos[9].nombre = "Snickers";
-            aProductos[9].precio = 50;
-            aProductos[9].existencia = 10;
-        }
-
-        public void compra()
-        {
-            Console.WriteLine("0- Galleta");
-            Console.WriteLine("1- Agua");
-            Console.WriteLine("2- Jugo");
-            Console.WriteLine("3- Refresco");
-            Console.WriteLine("4- Frito Lay");
-            Console.WriteLine("5- 360 Energy");
-            Console.WriteLine("6- Vive100");
-            Console.WriteLine("7- Mani");
-            Console.WriteLine("8- Hershey's");
-            Console.WriteLine("9- Snickers");
-
-            Console.Write("Escriba la opcion del producto que desea comprar: ");
-            opcompra = int.Parse(Console.ReadLine());
-            
-            for (int i = 0; i < aProductos.Length; i++)
-            {
-                if (opcompra == aProductos[i].opcion)
-                {
-                    Console.WriteLine("Su precio es: $" + aProductos[i].precio);
-                    
-                    Console.WriteLine("Ingrese su dinero: ");
-                    int pago = int.Parse(Console.ReadLine());
-
-                    if (pago == 5 || pago == 10 || pago == 25 || pago == 50 || pago == 100 || pago == 200)
-                    {
-                        aProductos[i].existencia = aProductos[1].existencia - 1;
-                        
-                        if (pago < aProductos[i].precio)
-                        {
-                            Console.WriteLine("Te faltan ", aProductos[i].precio - pago);
-                            pago = pago + int.Parse(Console.ReadLine());
-                        }
-                    } else{
-                        Console.WriteLine("Solo se aceptan monedas de 5, 10 y 25");
-                        Console.WriteLine("Solo se aceptan billetes de 50, 100 y 200");
-                    }
-                }
-            }
-        }
-
-    }
-    
-    class Maquina : Productos
-    {
-        private int menu_maq;
-        public void menu()
-        {
-            do
-            {
-                Console.WriteLine("\n1- Comprar");
-                Console.WriteLine("2- Mostrar Informe");
-                Console.WriteLine("3- Salir");
-
-                Console.Write("\nEscoja una opcion: ");
-                menu_maq = int.Parse(Console.ReadLine());
-
-                switch (menu_maq)
-                {
-                    case 1 : 
-                        compra();
-                    break;
-                    
-                    case 2 : 
-                    
-                    break;
-
-                    case 3 : 
-                        Console.Clear();
-                        Console.WriteLine("Finalizando Programa...");
-                    break;
-
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Caracter ingresado no valido");
-                    break;
-                }
-            } while (menu_maq != 3);
-            
-        }
         static void Main(string[] args)
         {
-            Maquina maq = new Maquina();
-            maq.menu();
+            Console.Clear();
+
+            producto producto1 = new producto();
+            producto1.nombre = "Galletas";
+            producto1.precio = 5;
+            producto1.existencia = 10;
+
+            producto producto2 = new producto();
+            producto2.nombre = "Frito Lay";
+            producto2.precio = 25;
+            producto2.existencia = 10;
+
+            producto producto3 = new producto();
+            producto3.nombre = "Hershey's";
+            producto3.precio = 50;
+            producto3.existencia = 10;
+
+            producto producto4 = new producto();
+            producto4.nombre = "Agua";
+            producto4.precio = 10;
+            producto4.existencia = 10;
+
+            producto producto5 = new producto();
+            producto5.nombre = "Jugo";
+            producto5.precio = 50;
+            producto5.existencia = 10;
+
+            producto producto6 = new producto();
+            producto6.nombre = "Refresco";
+            producto6.precio = 15;
+            producto6.existencia = 10;
+
+            producto producto7 = new producto();
+            producto7.nombre = "Snickers";
+            producto7.precio = 50;
+            producto7.existencia = 10;
+
+            producto producto8 = new producto();
+            producto8.nombre = "Many";
+            producto8.precio = 50;
+            producto8.existencia = 10;
+
+            producto producto9 = new producto();
+            producto9.nombre = "Vive100";
+            producto9.precio = 100;
+            producto9.existencia = 10;
+
+            producto producto10 = new producto();
+            producto10.nombre = "Energy 360";
+            producto10.precio = 100;
+            producto10.existencia = 10;
+    
+        
+        int dinero, menu,devuelta,Ganancia = 0;
+        int ventagalleta = 0, ventaFritoLay = 0,ventasHershey = 0,ventagua = 0,ventejugo = 0,ventarefresco = 0,
+        ventaSnickers = 0, ventaMany = 0, ventaVive100 = 0, ventareEnergy = 0;
+        
+        do
+        {
+            Console.WriteLine("\n------ Maquina Dispensadora de Alimentos ------");   
+            Console.WriteLine("Solo aceptamos monedas de 5,10,25 y billetes de 50, 100 y 200 pesos");
+            Console.Write("\nIntroduzca dinero: ");
+            dinero = int.Parse(Console.ReadLine());
+       
+
+            if (dinero == 5 || dinero == 10 || dinero == 25 || dinero == 50 || dinero== 100|| dinero == 200)
+            {
+            Console.WriteLine("\nUsted introdijo $" + dinero );
+            Console.WriteLine("Que comprará ?\n");
+            Console.WriteLine("1- Galletas = ${0}" , producto1.precio  );
+            Console.WriteLine("2- Frito Lay = ${0}", producto2.precio);
+            Console.WriteLine("3- Hershey's = ${0}", producto3.precio);
+            Console.WriteLine("4- Agua = ${0}", producto4.precio);
+            Console.WriteLine("5- Jugo = ${0}", producto5.precio);
+            Console.WriteLine("6- Refresco = ${0}", producto6.precio);
+            Console.WriteLine("7- Snickers = ${0}", producto7.precio);
+            Console.WriteLine("8- Many = ${0}", producto8.precio);
+            Console.WriteLine("9- Vive100 = ${0}", producto9.precio);
+            Console.WriteLine("10- Energy 360 = ${0}", producto10.precio);
+            Console.WriteLine("11- Registro de venta y Salir");
+            Console.Write("\nEscriba el # producto que quiere: ");
+            menu = int.Parse(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 1:
+                if (dinero >= producto1.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto1.nombre);
+                devuelta = dinero - producto1.precio;
+                Ganancia += producto1.precio;
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto1.existencia -= 1;
+                ventagalleta += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+
+                break;
+                case 2:
+                if (dinero >= producto2.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto2.nombre);
+                devuelta = dinero - producto2.precio;
+                Ganancia += producto2.precio;
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto2.existencia -= 1;
+                ventaFritoLay += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 3:
+                if (dinero >= producto3.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto3.nombre);
+                devuelta = dinero - producto3.precio;
+                Ganancia += producto3.precio;
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto3.existencia -= 1;
+                ventasHershey += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 4:
+                if (dinero >= producto4.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto4.nombre);
+                devuelta = dinero - producto4.precio;
+                Ganancia += producto4.precio;
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto4.existencia -= 1;
+                ventagua += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 5:
+                if (dinero >= producto5.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto5.nombre);
+                devuelta = dinero - producto5.precio;
+                Ganancia += producto5.precio;
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto5.existencia -= 1;
+                ventejugo += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 6:
+                if (dinero >= producto6.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto6.nombre);
+                devuelta = dinero - producto6.precio;
+                Ganancia += producto6.precio; 
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto6.existencia -= 1;
+                ventarefresco += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 7:
+                if (dinero >= producto7.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto7.nombre);
+                devuelta = dinero - producto7.precio;
+                Ganancia += producto7.precio; 
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto7.existencia -= 1;
+                ventaSnickers += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 8:
+                if (dinero >= producto8.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto8.nombre);
+                devuelta = dinero - producto8.precio;
+                Ganancia += producto8.precio; 
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto8.existencia -= 1;
+                ventaMany += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 9:
+                if (dinero >= producto9.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto9.nombre);
+                devuelta = dinero - producto9.precio;
+                Ganancia += producto9.precio; 
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto9.existencia -= 1;
+                ventaVive100 += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 10:
+                if (dinero >= producto10.precio)
+                {
+                Console.WriteLine("Entregando tu " + producto10.nombre);
+                devuelta = dinero - producto10.precio;
+                Ganancia += producto10.precio; 
+                Console.WriteLine("Su devuelta es: " + devuelta);
+                producto10.existencia -= 1;
+                ventareEnergy += 1;
+                Console.WriteLine("Gracias por si compra!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("El dinero no le alcanza para comprar este producto");
+                }
+        
+                break;
+                case 11:
+                Console.Clear();
+                Console.WriteLine("En el dia se vendieron " + ventagalleta +"Galletas, "+ ventaFritoLay + "Frito Lay, " + ventasHershey
+                + " Hershey, " + ventagua + " Agua, "+ ventejugo + "Jugos, " + ventarefresco+ "Refresco, " + ventaSnickers 
+                + "Snickers, " + ventaMany + "ventaMany, " + ventaVive100 + "Vive100, " + ventareEnergy + "Energy 360");
+                Console.WriteLine(" Y " + Ganancia+ " pesos de ganancias");
+                Console.WriteLine("Programa Finalizado");
+                break;
+            }
+            } else
+            {
+                Console.WriteLine("No hacectamos esa cantidad de dinero");
+            }
+        } while(true);
         }
     }
 }
